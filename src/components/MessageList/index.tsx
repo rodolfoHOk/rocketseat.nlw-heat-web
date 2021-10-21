62770c88799c0c7ada8bf16f4bb91795f93e36be
rodolfoHOk <rodolfo.okino@gmail.com>
2021-10-21 11:46:30 -0300

    add clearInterval in useEffect

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -25,7 +25,7 @@ export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       if (messagesQueue.length > 0) {
         setMessages((prevState) =>
           [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
@@ -34,6 +34,8 @@ export function MessageList() {
         messagesQueue.shift();
       }
     }, 3000);
+
+    return () => clearInterval(timer);
   }, []);
 
   useEffect(() => {
